cc95d9e99141e15e981a507a00b65e5f3e8bb400
fwlpe <fwlpe@VirtualDebian.localdomain>
2019-03-29 09:38:54 +0300

    new commit

--- fractol.h
+++ fractol.h
@@ -6,15 +6,19 @@
 /*   By: fwlpe <fwlpe@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/28 13:34:44 by fwlpe             #+#    #+#             */
-/*   Updated: 2019/03/28 14:09:51 by fwlpe            ###   ########.fr       */
+/*   Updated: 2019/03/28 19:21:29 by fwlpe            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FDF_H
 # define FDF_H
 
-# include "minilibx/mlx.h"
+# define W 1000
+# define H 1000
+
+# include <math.h>
 # include "libft/libft.h"
+# include "minilibx/mlx.h"
 
 typedef struct	s_mlx
 {	
--- main.c
+++ main.c
@@ -6,12 +6,44 @@
 /*   By: fwlpe <fwlpe@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/28 13:11:04 by fwlpe             #+#    #+#             */
-/*   Updated: 2019/03/28 14:29:55 by fwlpe            ###   ########.fr       */
+/*   Updated: 2019/03/28 21:09:44 by fwlpe            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "fractol.h"
+
+int	is_white(float r, float im, float r_start, float im_start, int depth)
+{
+	float	r_new;
+	float	im_new;
+	
+	if ((r * r + im * im) > 4)
+		return (0);
+	if (!depth)
+		return (1);
+	r_new = r * r - im * im + r_start;
+	im_new = 2 * r * im + im_start;
+	return (is_white(r_new, im_new, r_start, im_start, --depth));
+}
+
+void	field_iter(t_mlx *mlx)
+{
+	int i;
+	int j;
+
+	float scale = H / 5;
+	i = -1;
+	while (++i < W)
+	{
+		j = -1;
+		while (++j < H)
+		{
+			if (is_white(0, 0, (float)i / scale - W / scale / 2, (float)j / scale - H / scale / 2, 255))
+				mlx_pixel_put(mlx->mlx_ptr, mlx->win_ptr, i, j, 0xF5DEB3);
+		}
+	}
+}
 
 int	main(int ac, char **av)
 {
@@ -19,9 +51,10 @@ int	main(int ac, char **av)
 	(void)av;
 	t_mlx mlx;
 
-	printf("Hello World!\n");
+	// printf("Hello World!\n");
 	mlx.mlx_ptr = mlx_init();
-	mlx.win_ptr = mlx_new_window(mlx.mlx_ptr, 1024, 768, "Whooy");
+	mlx.win_ptr = mlx_new_window(mlx.mlx_ptr, W, H, "Whooy");
+	field_iter(&mlx);
 	mlx_loop(mlx.mlx_ptr);
 	return (0);
 }
