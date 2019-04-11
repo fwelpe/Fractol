ae1a54b848ee6351161d8dd539321edcb519bd6d
Crawford Denys arryn <cdenys-a@il-j3.21-school.ru>
2019-04-11 14:26:20 +0300

    no old files; no mlx_loop_hook (only mousehook) for julia now

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fwlpe <fwlpe@student.42.fr>                +#+  +:+       +#+        */
+/*   By: cdenys-a <cdenys-a@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/28 13:11:04 by fwlpe             #+#    #+#             */
-/*   Updated: 2019/04/09 20:22:56 by fwlpe            ###   ########.fr       */
+/*   Updated: 2019/04/11 14:24:33 by cdenys-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,6 @@ int		main(int ac, char **av)
 	mlx_key_hook(s.win_ptr, deal_key, &s);
 	mlx_mouse_hook(s.win_ptr, mouse, (void *)&s);
 	mlx_hook(s.win_ptr, DELETE_WINDOW, 0, red_button, NULL);
-	if (ft_strequ(s.f_name, "julia"))
-		mlx_loop_hook(s.mlx_ptr, (void *)draw, (void *)&s);
 	mlx_hook(s.win_ptr, 6, 0, mouse_move, (void *)&s);
 	mlx_loop(s.mlx_ptr);
 	return (0);
